ce928e0f8c34e9b12fe0d21c6280363fe5a50e28
huyhai1994 <nguyenhuyhai926@gmail.com>
2025-08-10 20:24:27 +0700

    add new test case for PhoneBook

--- src/test/java/spring/learning/session1/PhoneBookTest.java
+++ src/test/java/spring/learning/session1/PhoneBookTest.java
@@ -1,6 +1,5 @@
 package spring.learning.session1;
 
-
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -8,11 +7,18 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class PhoneBookTest {
 
-
     @Test
     @DisplayName("test getName()")
     void getName() {
         PhoneBook phonebook = new PhoneBook("abc", "555-4444", "abc@gmail.com");
         assertEquals("abc", phonebook.getName());
+    }
+
+    @Test
+    @DisplayName("test getEmail()")
+    void getEmail() {
+        PhoneBook phonebook = new PhoneBook("abc", "555-4444", "abc@gmail.com");
+        System.out.println("new code here!!!");
+        assertEquals("abc@gmail.com", phonebook.getEmail());
     }
 }
