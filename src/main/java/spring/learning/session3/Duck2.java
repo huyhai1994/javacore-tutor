06963d4f1252e953460880df7c7d24bbf81f02e6
huyhai1994 <nguyenhuyhai926@gmail.com>
2025-08-12 06:41:32 +0700

    overloading concepts

---
+++ src/main/java/spring/learning/session3/Duck2.java
@@ -0,0 +1,13 @@
+package spring.learning.session3;
+
+public class Duck2 {
+    int size;
+
+    public Duck2(int size) {
+        this.size = size;
+    }
+
+    public Duck2() {
+        size = 27;
+    }
+}
