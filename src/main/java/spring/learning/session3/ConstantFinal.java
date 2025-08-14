e93810a3bcdbf0b1f1727bc3dc085e987ff6a7f6
huyhai1994 <nguyenhuyhai926@gmail.com>
2025-08-14 12:03:59 +0700

    final keyword

---
+++ src/main/java/spring/learning/session3/ConstantFinal.java
@@ -0,0 +1,5 @@
+package spring.learning.session3;
+
+public class ConstantFinal {
+    final static int SIZE = 3;
+}
--- src/test/java/spring/learning/session3/PlayerTest.java
+++ src/test/java/spring/learning/session3/PlayerTest.java
@@ -2,6 +2,9 @@ package spring.learning.session3;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class PlayerTest {
@@ -11,12 +14,12 @@ class PlayerTest {
         assertEquals(0, Player.playerCount);
     }
 
-
     @Test
     void testPlayerCount10() {
         for (int i = 0; i < 10; i++) {
             new Player();
         }
+        List<Player> players  = new ArrayList<>();
         assertEquals(10, Player.playerCount);
     }
 }
