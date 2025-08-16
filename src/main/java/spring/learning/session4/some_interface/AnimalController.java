7f8f505e2c537dd608bef3d4ca2cf3ea5f2824eb
huyhai1994 <nguyenhuyhai926@gmail.com>
2025-08-16 16:33:32 +0700

    feat: implement fetchAnimal method and add Cat class with IAnimal interface

--- src/main/java/spring/learning/session4/some_interface/AnimalController.java
+++ src/main/java/spring/learning/session4/some_interface/AnimalController.java
@@ -1,5 +1,10 @@
 package spring.learning.session4.some_interface;
 
+import java.util.List;
+
 public class AnimalController {
-    IAnimal animal;
+
+    public static <T extends IAnimal> void fetchAnimal(List<T> animals) {
+        animals.forEach(IAnimal::makenoise);
+    }
 }
