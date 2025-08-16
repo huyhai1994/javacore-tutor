57cbbb0a5235f66982c0bd834ea1c89407a94192
huyhai1994 <nguyenhuyhai926@gmail.com>
2025-08-16 15:26:34 +0700

    feat: update DiamondOperator to use diamond operator and add PolymorphismExample class

--- src/main/java/spring/learning/session7/diamond_operator/DiamondOperator.java
+++ src/main/java/spring/learning/session7/diamond_operator/DiamondOperator.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 public class DiamondOperator {
     public static void main(String[] args) {
-        List<String> cars = new ArrayList<String>(List.of("Ford", "Vinfast"));
+        List<String> cars = new ArrayList<>(List.of("Ford", "Vinfast"));
         cars.forEach(System.out::println);
     }
 }
