630b40646a69a9938739481359b22cd78dc2bf03
huyhai1994 <nguyenhuyhai926@gmail.com>
2025-08-27 12:36:47 +0700

    create a new thread that do not thing

---
+++ src/main/java/spring/learning/session8/DemoThread.java
@@ -0,0 +1,8 @@
+package spring.learning.session8;
+
+public class DemoThread {
+    public static void main(String[] args) {
+        Thread thread = new Thread();
+        thread.start();
+    }
+}
